801514e561ed497a17543855a025bf561abd0488
yoichi0615 <aaa11655pt@gmail.com>
2022-05-26 22:13:48 +0900

    ts型学習

--- index.js
+++ index.js
@@ -1,3 +1,78 @@
-var hello = "hello";
-console.log(hello);
-
+var hasValue = false;
+var count = 10;
+var float = 3.14;
+var negative = -0.12;
+var single = 'hello';
+var double = 'hello';
+var back = 'hello';
+var hello;
+hello = 'hello';
+var person = {
+    name: 'jack',
+    age: 21
+};
+// console.log(person.name)
+var fruits = ['Apple', 'Banana', 'Grape'];
+var book = ['business', 1, false];
+book.push(21);
+// console.log(book[3]);
+var CoffeeSize;
+(function (CoffeeSize) {
+    CoffeeSize["SMALL"] = "SMALL";
+    CoffeeSize["SHORT"] = "SHORT";
+    CoffeeSize["TALL"] = "TALL";
+    CoffeeSize["VENTI"] = "VENTI";
+})(CoffeeSize || (CoffeeSize = {}));
+var coffee = {
+    hot: true,
+    size: CoffeeSize.VENTI
+};
+coffee.size = CoffeeSize.TALL;
+var anything = true;
+anything = 'hello';
+anything = ['hello', 33, true];
+anything = {};
+anything.jfafafa = 'fafafa';
+var banana = anything;
+var unionType = 10;
+var unionTypes = ['apple', 765];
+var apple = 'apple';
+var clothSize = 'large';
+var cloth = {
+    color: 'blue',
+    size: 'medium'
+};
+function add(num1, num2) {
+    return num1 + num2;
+}
+function sayHello() {
+    console.log('hello');
+}
+console.log(sayHello());
+var tmp = null;
+var tmpNull = undefined;
+var anotherAdd = function (num1, num2) {
+    return num1 + num2;
+};
+var doubleNumber = function (num) { return num * 2; };
+function doubleAndHandle(num, cb) {
+    var doubleNumber = cb(num * 2);
+    console.log(num * 2);
+}
+doubleAndHandle(21, function (doubleNum) {
+    return doubleNum;
+});
+var unknownInput;
+var anyInput;
+var text;
+unknownInput = 1;
+unknownInput = false;
+unknownInput = "string";
+text = anyInput;
+if (typeof unknownInput === 'string') {
+    text = unknownInput;
+}
+function error(message) {
+    throw new Error(message);
+}
+console.log(error('This is an error'));
--- index.ts
+++ index.ts
@@ -19,7 +19,7 @@ const person: object = {
 const fruits: any[] = ['Apple', 'Banana', 'Grape']
 const book: [string, number, boolean] = ['business', 1 , false];
 book.push(21);
-console.log(book[3]);
+// console.log(book[3]);
 
 enum CoffeeSize {
   SMALL = 'SMALL',
@@ -34,3 +34,66 @@ const coffee = {
 }
 
 coffee.size = CoffeeSize.TALL;
+
+let anything: any = true;
+anything = 'hello';
+anything = ['hello', 33, true];
+anything = {};
+anything.jfafafa = 'fafafa';
+let banana = anything;
+
+let unionType: number | string = 10;
+let unionTypes: (number | string)[] = ['apple', 765]
+
+type ClothSize = 'small' | 'medium' | 'large'
+const apple = 'apple';
+let clothSize: ClothSize = 'large'
+const cloth: {
+  color: string,
+  size: ClothSize
+} = {
+  color: 'blue',
+  size: 'medium'
+};
+
+function add(num1: number, num2: number): number {
+  return num1 + num2
+}
+
+function sayHello(): void {
+  console.log('hello');
+}
+
+console.log(sayHello())
+let tmp: undefined = null;
+let tmpNull: null = undefined;
+
+const anotherAdd: (n1: number, n2: number) => number = function (num1, num2) {
+  return num1 + num2
+}
+const doubleNumber: (num: number) => number = num => num * 2;
+
+function doubleAndHandle(num: number, cb: (num: number) => number): void {
+  const doubleNumber = cb(num * 2)
+  console.log(num * 2);
+}
+
+doubleAndHandle(21, doubleNum => {
+  return doubleNum
+});
+
+let unknownInput: unknown;
+let anyInput: any;
+let text: string;
+unknownInput = 1;
+unknownInput = false;
+unknownInput = "string";
+text = anyInput;
+if (typeof unknownInput === 'string') {
+  text = unknownInput;
+}
+
+function error(message: string): never {
+  throw new Error(message);
+}
+console.log(error('This is an error'));
